8458ed4f5f0d3cbea198d93d9360b627f7223d65
Alex <alex.kingman@blackbaud.com>
2021-01-15 16:52:57 -0500

    Modern theme for vertical tabs (#150)
    * initial modern theme commit
    
    * e2e tests
    
    * fixed style
    
    * added css for different tab states
    
    * added borderless class to button
    
    * space
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>

--- e2e/vertical-tabs.e2e-spec.ts
+++ e2e/vertical-tabs.e2e-spec.ts
@@ -1,104 +1,174 @@
 import {
+  browser,
+  by,
+  element
+} from 'protractor';
+
+import {
   expect,
-  SkyHostBrowser
+  SkyHostBrowser,
+  SkyVisualThemeSelector
 } from '@skyux-sdk/e2e';
 
 import {
-  element,
-  by,
-  browser
-} from 'protractor';
+  SkyHostBrowserBreakpoint
+} from '@skyux-sdk/e2e/host-browser/host-browser-breakpoint';
 
 describe('Vertical Tabs', () => {
-  it('should match previous vertical tabset screenshot', (done) => {
-    SkyHostBrowser.get('visual/vertical-tabset');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    SkyHostBrowser.scrollTo('#screenshot-vertical-tabset');
-    expect('#screenshot-vertical-tabset').toMatchBaselineScreenshot(done, {
-      screenshotName: 'vertical-tabset'
+
+  //#region helpers
+  let browserSize: SkyHostBrowserBreakpoint;
+  let currentTheme: string;
+  let currentThemeMode: string;
+
+  async function selectTheme(theme: string, mode: string): Promise<void> {
+    currentTheme = theme;
+    currentThemeMode = mode;
+
+    return SkyVisualThemeSelector.selectTheme(theme, mode);
+  }
+
+  async function setBrowserSize(size: SkyHostBrowserBreakpoint): Promise<void> {
+    browserSize = size;
+
+    return SkyHostBrowser.setWindowBreakpoint(size);
+  }
+
+  function getScreenshotName(name: string): string {
+    if (browserSize) {
+      name += '-' + browserSize;
+    }
+
+    if (currentTheme) {
+      name += '-' + currentTheme;
+    }
+
+    if (currentThemeMode) {
+      name += '-' + currentThemeMode;
+    }
+
+    return name;
+  }
+
+  function runTests(): void {
+    it('should match previous vertical tabset screenshot', (done) => {
+      SkyHostBrowser.scrollTo('#screenshot-vertical-tabset');
+      expect('#screenshot-vertical-tabset').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('vertical-tabset')
+      });
+    });
+
+    it('should match previous vertical tabset screenshot after clicking tab', (done) => {
+      SkyHostBrowser.scrollTo('#screenshot-vertical-tabset');
+
+      if (browserSize === 'xs') {
+        const showTabsButton =
+          element(by.css('#screenshot-vertical-tabset .sky-vertical-tabset-show-tabs-btn'));
+        browser.wait(function () { return browser.isElementPresent(showTabsButton); }, 8000);
+
+        // show tabs
+        showTabsButton.click();
+      }
+
+      const groupElement = element(by.css('.group2'));
+      browser.wait(function () { return browser.isElementPresent(groupElement); }, 4000);
+
+      // open group
+      groupElement.click();
+
+      // click tab
+      element(by.id('group2Tab2')).click();
+      expect('#screenshot-vertical-tabset').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('vertical-tabset-clicked-tab')
+      });
+    });
+
+    if (browserSize === 'xs') {
+      it('should match previous vertical tabset screenshot after clicking show tabs', (done) => {
+        SkyHostBrowser.scrollTo('#screenshot-vertical-tabset');
+        const showTabsButton =
+          element(by.css('#screenshot-vertical-tabset .sky-vertical-tabset-show-tabs-btn'));
+        browser.wait(function () { return browser.isElementPresent(showTabsButton); }, 8000);
+
+        // show tabs
+        showTabsButton.click();
+
+        expect('#screenshot-vertical-tabset').toMatchBaselineScreenshot(done, {
+          screenshotName: getScreenshotName('vertical-tabset-mobile-show-tabs')
+        });
+      });
+    }
+
+    it('should match previous vertical tabset screenshot without groups', (done) => {
+      SkyHostBrowser.scrollTo('#screenshot-vertical-tabs-no-groups');
+      expect('#screenshot-vertical-tabs-no-groups').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('vertical-tabset-no-groups')
+      });
+    });
+
+    it('should match previous modal screenshot', (done) => {
+      SkyHostBrowser.scrollTo('#vertical-tabset-modal-launcher');
+      element(by.css('#vertical-tabset-modal-launcher')).click();
+      SkyHostBrowser.scrollTo('#screenshot-vertical-tabset-modal');
+      expect('#screenshot-vertical-tabset-modal').toMatchBaselineScreenshot(done, {
+        screenshotName: getScreenshotName('vertical-tabset-modal')
+      });
+    });
+  }
+  //#endregion
+
+  describe('(size: lg)', () => {
+    beforeEach( async() => {
+      currentTheme = undefined;
+      currentThemeMode = undefined;
+      await SkyHostBrowser.get('visual/vertical-tabset');
+      await setBrowserSize('lg');
+    });
+
+    runTests();
+
+    describe('when modern theme', () => {
+      beforeEach(async () => {
+        await selectTheme('modern', 'light');
+      });
+
+      runTests();
+    });
+
+    describe('when modern theme in dark mode', () => {
+      beforeEach(async () => {
+        await selectTheme('modern', 'dark');
+      });
+
+      runTests();
     });
   });
 
-  it('should match previous vertical tabset screenshot (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/vertical-tabset');
-    SkyHostBrowser.setWindowBreakpoint('xs');
-    SkyHostBrowser.scrollTo('#screenshot-vertical-tabset');
-    expect('#screenshot-vertical-tabset').toMatchBaselineScreenshot(done, {
-      screenshotName: 'vertical-tabset-xs'
+  describe('(size: xs)', () => {
+    beforeEach( async() => {
+      currentTheme = undefined;
+      currentThemeMode = undefined;
+      await SkyHostBrowser.get('visual/vertical-tabset');
+      await setBrowserSize('xs');
+    });
+
+    runTests();
+
+    describe('when modern theme', () => {
+      beforeEach(async () => {
+        await selectTheme('modern', 'light');
+      });
+
+      runTests();
+    });
+
+    describe('when modern theme in dark mode', () => {
+      beforeEach(async () => {
+        await selectTheme('modern', 'dark');
+      });
+
+      runTests();
     });
   });
 
-  it('should match previous vertical tabset screenshot after clicking tab', (done) => {
-    SkyHostBrowser.get('visual/vertical-tabset');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    SkyHostBrowser.scrollTo('#screenshot-vertical-tabset');
-    const groupElement = element(by.css('.group2'));
-    browser.wait(function () { return browser.isElementPresent(groupElement); }, 8000);
-
-    // open group
-    groupElement.click();
-
-    // click tab
-    element(by.id('group2Tab2')).click();
-    expect('#screenshot-vertical-tabset').toMatchBaselineScreenshot(done, {
-      screenshotName: 'vertical-tabset-clicked-tab'
-    });
-  });
-
-  it('should match previous vertical tabset screenshot after clicking tab (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/vertical-tabset');
-    SkyHostBrowser.setWindowBreakpoint('xs');
-    SkyHostBrowser.scrollTo('#screenshot-vertical-tabset');
-    const showTabsButton =
-      element(by.css('#screenshot-vertical-tabset .sky-vertical-tabset-show-tabs-btn'));
-    browser.wait(function () { return browser.isElementPresent(showTabsButton); }, 8000);
-
-    // show tabs
-    showTabsButton.click();
-
-    // open group
-    element(by.css('.group2')).click();
-
-    // click tab
-    element(by.id('group2Tab2')).click();
-    expect('#screenshot-vertical-tabset').toMatchBaselineScreenshot(done, {
-      screenshotName: 'vertical-tabset-xs-clicked-tab'
-    });
-  });
-
-  it('should match previous vertical tabset screenshot after clicking show tabs (screen: xs)', (done) => {
-    SkyHostBrowser.get('visual/vertical-tabset');
-    SkyHostBrowser.setWindowBreakpoint('xs');
-    SkyHostBrowser.scrollTo('#screenshot-vertical-tabset');
-    const showTabsButton =
-      element(by.css('#screenshot-vertical-tabset .sky-vertical-tabset-show-tabs-btn'));
-    browser.wait(function () { return browser.isElementPresent(showTabsButton); }, 8000);
-
-    // show tabs
-    showTabsButton.click();
-
-    expect('#screenshot-vertical-tabset').toMatchBaselineScreenshot(done, {
-      screenshotName: 'vertical-tabset-mobile-show-tabs'
-    });
-  });
-
-  it('should match previous vertical tabset screenshot without groups', (done) => {
-    SkyHostBrowser.get('visual/vertical-tabset');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    SkyHostBrowser.scrollTo('#screenshot-vertical-tabs-no-groups');
-    expect('#screenshot-vertical-tabs-no-groups').toMatchBaselineScreenshot(done, {
-      screenshotName: 'vertical-tabset-no-groups'
-    });
-  });
-
-  it('should match previous modal screenshot', (done) => {
-    SkyHostBrowser.get('visual/vertical-tabset');
-    SkyHostBrowser.setWindowBreakpoint('lg');
-    SkyHostBrowser.scrollTo('#vertical-tabset-modal-launcher');
-    element(by.css('#vertical-tabset-modal-launcher')).click();
-    SkyHostBrowser.scrollTo('#screenshot-vertical-tabset-modal');
-    expect('#screenshot-vertical-tabset-modal').toMatchBaselineScreenshot(done, {
-      screenshotName: 'vertical-tabset-modal'
-    });
-  });
 });
